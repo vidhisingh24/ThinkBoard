91eed872d029325d2e27c2ae4e5b392b71ad8187
Vidhi Singh <vidhiisingh2403@gmail.com>
2026-07-29 16:57:39 +0530

    remove MongoDB connection string example

--- backend/test.js
+++ backend/test.js
@@ -1,7 +1,5 @@
 import { MongoClient } from "mongodb";
 
-const uri =
-  "mongodb+srv://vidhiisingh2403_db_user:YOUR_PASSWORD@cluster0.dq3aojp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri);
 
