775e13eb3daeaa23c9a724602c688b7856600ec0
Vidhi Singh <vidhiisingh2403@gmail.com>
2026-07-29 20:42:30 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from 'express';
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -9,19 +10,30 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 
 const app = express();
+const __dirname = path.resolve()
 
 console.log(process.env.MONGO_URI);
 connectDB();
 //middleware
+if(process.env.NODE_ENV !== "production") {
 app.use(cors({
   origin:"http://localhost:5173",
-})); //Enable CORS for all routes
+})
+);
+} //Enable CORS for all routes
 
 app.use(express.json());
 app.use(rateLimiter); // Apply the rate limiter middleware to all routes
 
 
 app.use("/api/notes", notesRoutes);
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,43 @@
-import { Route, Routes } from 'react-router';
+import { Routes, Route } from "react-router-dom";
 
-import HomePage from './pages/HomePage';
-import CreatePage from './pages/CreatePage';
-import NoteDetailPage from './pages/NoteDetailPage';
-
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
 
 const App = () => {
   return (
-
-     <div className="relative min-h-screen overflow-hidden">
-
-      {/* Background */}
-      <div
-        className="fixed inset-0 -z-10"
-        style={{
-          background:
-            "radial-gradient(circle at 50% 10%, #ffffff 0%, #f5dcdc 35%, #c1121f 90%, #780000 100%)",
-        }}
-      />
-  
-    
-    <Routes> //return routes
-      <Route path="/" element={<HomePage />} /> //home page route
-      <Route path="/create" element={<CreatePage />} /> //create page route
-      <Route path="/note/:id" element={<NoteDetailPage />} /> //note detail page route
-    </Routes>
-
-  </div>
+    <div className="absolute inset-0 -z-10 h-full w-full [background:radial-gradient(125%_125%_at_50%_10%,#F5F5F5_40%,#FFFDD0_100%)]">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
   );
 };
-export default App;
+
+export default App;// import { Route, Routes } from 'react-router';
+
+// import HomePage from './pages/HomePage';
+// import CreatePage from './pages/CreatePage';
+// import NoteDetailPage from './pages/NoteDetailPage';
+
+
+// const App = () => {
+//   return (
+//     <div className="absolute inset-0 -z-10 h-full w-full [background:radial-gradient(125%_125%_at_50%_10%,#F5F5F5_40%,#FFFDD0_100%)]/>
+
+        
+//    <Routes>
+//   {/* Home page route */}
+//   <Route path="/" element={<HomePage />};
+//     < Route path = "/create" element = {< CreatePage />};
+// <Route path="/note/:id" element={<NoteDetailPage />};
+// </ Routes>
+
+// </div>
+//   );
+// };
+// export default App;
 
 // if u visit create page through path so through element i would like to the create page className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#F5F5F5_40%,#FFFDD0_100%)]"
