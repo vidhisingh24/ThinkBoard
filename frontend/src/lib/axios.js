20c68551802d8a78cb9e2427d5d91f8112846333
Vidhi Singh <vidhiisingh2403@gmail.com>
2026-07-29 21:18:45 +0530

    Fix axios syntax error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const Base_URL = import.meta.env.MODE === "development ? "http://localhost:5001/api" :"/api"
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "/api";
+
 const api = axios.create({
-    baseURL: BASE_URL,
+  baseURL: BASE_URL,
 });
 
 export default api;
